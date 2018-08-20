e43ccd986edb3c82808e26603866e4be5f0d582a
Jonathan Hong <jonathan@panoply.io>
2018-08-19 17:45:24 -0700

    Fixed missing pattern matching syntax, and added additional functions that include pattern matching

--- src/main/2errorhandling/ErrorHandling.scala
+++ src/main/2errorhandling/ErrorHandling.scala
@@ -8,7 +8,7 @@ trait Option[+A] {
     }
 
     // (default: => B) syntax: the fn will not be evaluated until needed by the fn
-    def getOrElse[B >: A](default: => B): B = {
+    def getOrElse[B >: A](default: => B): B = this match {
         case None => default
         case Some(a) => a
     }
@@ -17,16 +17,28 @@ trait Option[+A] {
         map(f) getOrElse None
     }
 
+    // implementation using pattern matching
+    def flatMap2[B](f: A => B): Option[A] = this match {
+        case None => None
+        case Some(a) => f(a)
+    }
+
     // [B >: A] syntax: indicates that B must be a supertype of A
     def orElse[B >: A](ob: => Option[B]): Option[B] = {
         this map (Some(_)) getOrElse ob
     }
 
-    def filter(f: A => Boolean): Option[A] = {
+    // implementation using pattern matching
+    def filter(f: A => Boolean): Option[A] = this match {
         case Some(a) if f(a) => this
         case _ => None
     }
 
+    // implementation using flatMap
+    def filter2(f: A => Boolean): Option[A] = {
+        flatMap(a => if f(a) Some(a) else None)
+    }
 }
+
 class object Some[+A](get: A): extends Option[A]
 object None extends Option[Nothing]
